d9efd246344056c0c8ceebea2d4516eea7f80bf4
mayankshah1607 <mayankshah1614@gmail.com>
2019-02-22 11:40:01 +0530

    Debuggin on server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 
 
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 5000;
 
 const regsModel = require('./Models/regs');
 
@@ -15,7 +15,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+// app.use(cors())
 
 
 
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_DB_URL, (err) => {
 
 
 app.post('/register',(req,res) => {
-
+    console.log(req.headers.origin)
     validationData = Validate(req.body)
 
     if (validationData.Status == "Failed"){
@@ -48,32 +48,32 @@ app.post('/register',(req,res) => {
 
 })
 
-// var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function(req, res, next) {
 
-//     var allowedOrigins = ['http://ith.ieeevit.com'];
+    var allowedOrigins = ['http://ith.ieeevit.com'];
 
-//     var origin = req.headers.origin;
-//     if(allowedOrigins.indexOf(origin) > -1){
-//         res.header('Access-Control-Allow-Origin', origin);
-//     }
-//     res.header('Connection','keep-alive');
-//     res.header('Keep-Alive','timeout=200');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, captchaCookie");
-//     res.header('content-type', 'application/json');
-//     res.header('Access-Control-Allow-Credentials', true);
-//     if (req.method === 'OPTIONS') {
-//         var headers = {
-//             "Access-Control-Allow-Methods" : "GET, POST, OPTIONS",
-//             "Access-Control-Allow-Credentials" : true
-//         };
-//         res.writeHead(200, headers);
-//         res.end();
-//     } else {
-//         next();
-//     }
-// }
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Connection','keep-alive');
+    res.header('Keep-Alive','timeout=200');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, captchaCookie");
+    res.header('content-type', 'application/json');
+    res.header('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+        var headers = {
+            "Access-Control-Allow-Methods" : "GET, POST, OPTIONS",
+            "Access-Control-Allow-Credentials" : true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    } else {
+        next();
+    }
+}
 
-// app.use(allowCrossDomain);
+app.use(allowCrossDomain);
 
 app.listen(port, () => {
     console.log(`App started on port: ${port}.`);
