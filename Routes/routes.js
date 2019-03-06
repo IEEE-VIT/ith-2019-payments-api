f21a46f9efbf6cb5ddfdbcba89322accdaf29baa
mayankshah1607 <mayankshah1614@gmail.com>
2019-03-06 19:39:23 +0530

    Making payment portal

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const _ = require('lodash');
 const Validate = require('../Utils/Validate');
-
+const request = require('request');
 const regsModel = require('../Models/regs');
+require('dotenv').config();
 
 const router = express.Router();
 
@@ -31,9 +32,42 @@ router.post('/register',(req,res) => {
 
 })
 
+
+router.get('/payment',(req,res) => {
+
+    request({
+        url: 'https://academics.vit.ac.in/online_application2/onlinepayment/Online_pay_request1.asp',
+        rejectUnauthorized: false,
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'dnt': "1",
+            'accept-encoding': "gzip, deflate, br",
+            'accept-languade': 'en-US,en,q=0.9',
+            'cache-control': 'no-cache'
+        },
+        form: {
+            id_event: process.env.id_event,
+            id_merchant: process.env.id_merchant,
+            id_password: process.env.id_password,
+            id_trans: 'IETE123123',
+            id_name: 'Mayank',
+            amt_event: 1.00
+        }
+    }, function (error,response,body){
+        if (error){
+            res.send(error)
+        }
+        res.send(body)
+
+    })
+    
+})
+
+
 router.post('/payment/status', (req,res) => {
     res.send({
-        Message: "Route works"
+        req
     })
 })
 
