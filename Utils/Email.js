804d799200cffb7982667f939dd72ad03c0026e8
mayankshah1607 <mayankshah1614@gmail.com>
2019-03-09 16:47:26 +0530

    Changed Email

--- Utils/Email.js
+++ Utils/Email.js
@@ -17,40 +17,15 @@ const sendMail = (to,ref,name) => {
         to: [to],
         from: '"Team ITH" <noreply@ieeevit.com>',
         subject: 'ITH 2019',
-        html: `
-               <html>
-               <body>
-               <div style="
-                background-color: #D8D8D8;
-                padding: 36px;
-              ">
-                <div
-                    style="
-                        padding: 24px;
-                        background-color: #fff;
-                        color: #000;
-                        border-radius: 3px;
-                        text-align: center;
-                    "
-                >
-                <p><strong>Hi ${name},</strong></p>
-                <p>Thank you for registering for ITH 2019.</p>
-                <p>Your Reference ID is : <strong>${ref}</strong></p><br/>
-                <div style="text-align: left;">
-                <p>With regards,</p>
-                <p>IEEE-VIT</p>
-                </div>
-                </div>
-                <p
-                style="
-                font-size: 12px;
-                color:#909090;"
-                >Gorbachev Rd, Vellore, Tamil Nadu 632014</p>
-              </div>
-               </body>
-               </html> 
-              
-              `
+        text: `
+            Hi, ${name}\n
+            You have successfully registered for ITH 2019.
+            Your reference ID is : ${ref}
+
+            With Regards,
+            IEEE-VIT
+
+        `
     };
      
     mailer.sendMail(email, function(err, res) {
